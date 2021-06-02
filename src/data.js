0b8fd73ad4ca00f128c501c518ffb091a1b5df65
Filip Katusin <filip.katusin@gmail.com>
2021-06-02 20:11:02 +0200

    pridanie zabudnutych dat pre fotky

---
+++ src/data.js
@@ -0,0 +1,29 @@
+import image1 from "./images/desktop-image-hero-1.jpg";
+import image2 from "./images/desktop-image-hero-2.jpg";
+import image3 from "./images/desktop-image-hero-3.jpg";
+import image1Mobile from "./images/mobile-image-hero-1.jpg";
+import image2Mobile from "./images/mobile-image-hero-2.jpg";
+import image3Mobile from "./images/mobile-image-hero-3.jpg";
+
+const heroes = [
+  {
+    title: "Discover innovative ways to decorate",
+    text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    image_desktop: image1,
+    image_mobile: image1Mobile,
+  },
+  {
+    title: "We are available all across the globe",
+    text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    image_desktop: image2,
+    image_mobile: image2Mobile,
+  },
+  {
+    title: "Manufactured with the best materials",
+    text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+    image_desktop: image3,
+    image_mobile: image3Mobile,
+  },
+];
+
+export default heroes;
